91a7327ae4edd3d39f238789b894828de5c36b62
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-06-23 13:19:31 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+const newTaskForm = document.getElementById("create-task-form")
+
+
+newTaskForm.addEventListener("submit", createNewTask)
+})
+
+function createNewTask(event){
+  const newTaskDescription = document.getElementById("new-task-description")
+  const newTaskLi = document.createElement("li")
+  newTaskLi.innerText = newTaskDescription.value
+  appendNewTaskLi(newTaskLi)
+  event.preventDefault()
+}
+
+function appendNewTaskLi(task){
+document.getElementById("tasks").appendChild(task)
+}
